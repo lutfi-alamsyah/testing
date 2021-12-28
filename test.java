bc63f39378ee3ea9c32204b8b5b2094090da4726
Lutfi alamsyah <lutfialamsyah1003@gmail.com>
2021-12-28 13:19:20 +0700

    adding word

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class Test {
 	String name = "lutfi alamsyah";
 	public static void main(String[] args) {
-		System.out.println("hello " + name);
+		System.out.println("hello " + name + " in testing");
 	}
 }
